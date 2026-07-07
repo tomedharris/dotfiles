f93d7673ff89d6b0bd0542de28b2eec807928151
Tom Harris <mrtom.h.84@gmail.com>
2026-07-07 16:35:52 +0100

    Dotfiles

--- nvim/lua/tomedharris/remap.lua
+++ nvim/lua/tomedharris/remap.lua
@@ -9,7 +9,16 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
 
 -- Map ctrl-hjkl panel navigation
-vim.keymap.set('n', '<C-J>', '<C-W><C-J>', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-K>', '<C-W><C-K>', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-L>', '<C-W><C-L>', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-H>', '<C-W><C-H>', { noremap = true, silent = true })
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>", { noremap = true, silent = true })
+
+-- Same hjkl window navigation from terminal-mode
+vim.keymap.set("t", "<C-J>", [[<C-\><C-n><C-W><C-J>]], { noremap = true, silent = true })
+vim.keymap.set("t", "<C-K>", [[<C-\><C-n><C-W><C-K>]], { noremap = true, silent = true })
+vim.keymap.set("t", "<C-L>", [[<C-\><C-n><C-W><C-L>]], { noremap = true, silent = true })
+vim.keymap.set("t", "<C-H>", [[<C-\><C-n><C-W><C-H>]], { noremap = true, silent = true })
+
+-- Map Leader n to nohl
+vim.keymap.set("n", "<leader>n", vim.cmd.nohl, { silent = true })
